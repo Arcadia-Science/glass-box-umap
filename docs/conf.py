ba285f4f73502247905cdc1f462cc0c108923c58
Evan Kiefl <kiefl.evan@gmail.com>
2025-12-03 13:51:32 -0800

    Changes requested in https://github.com/Arcadia-Science/glass-box-umap/pull/1

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath("../"))
 
 # -- Project information -----------------------------------------------------
 
-project = "[glass-box-umap]"
+project = "glass-box-umap"
 # copyright = "2024, Arcadia Science"
 author = "[FIRST] [LAST]"
 
@@ -107,7 +107,7 @@ autodoc_class_signature = "mixed"
 autoclass_content = "class"
 
 autoapi_type = "python"
-autoapi_dirs = ["../[glass_box_umap]"]
+autoapi_dirs = ["../glass_box_umap"]
 autoapi_keep_files = True
 
 autoapi_ignore = [
