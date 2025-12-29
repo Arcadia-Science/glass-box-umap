008267b21d82b9f84a1dc6157308e99bae28001f
Evan Kiefl <kiefl.evan@gmail.com>
2025-12-29 10:14:39 -0800

    Store input dims in lightning module, add neg sampling rate

--- src/glass_box_umap/parametric_umap/lightning/system.py
+++ src/glass_box_umap/parametric_umap/lightning/system.py
@@ -15,20 +15,33 @@ class UMAPLightningModule(pl.LightningModule):
     2. Matching mode to reproduce non-parametric UMAP embeddings
 
     Args:
-        lr: Learning rate for the AdamW optimizer.
-        encoder: Neural network encoder mapping data to embedding space.
-        min_dist: UMAP min_dist parameter for computing a and b.
+        lr:
+            Learning rate for the AdamW optimizer.
+        encoder:
+            Neural network encoder mapping data to embedding space.
+        input_dims:
+            The expected input shape (excluding batch size). Stored for validation
+            during inference/refitting.
+        min_dist:
+            UMAP min_dist parameter for computing a and b.
     """
 
     def __init__(
         self,
         lr: float,
         encoder: nn.Module,
+        input_dims: tuple[int, ...],
         min_dist: float = 0.1,
+        negative_sample_rate: int = 5,
     ) -> None:
         super().__init__()
+        self.save_hyperparameters(ignore=['encoder'])
+
         self.lr = lr
         self.encoder = encoder
+        self.input_dims = input_dims
+        self.negative_sample_rate = negative_sample_rate
+        
         self._a, self._b = find_ab_params(1.0, min_dist)
 
     def configure_optimizers(self) -> Optimizer:
@@ -44,7 +57,7 @@ class UMAPLightningModule(pl.LightningModule):
             embedding_from,
             self._a,
             self._b,
-            negative_sample_rate=5,
+            negative_sample_rate=self.negative_sample_rate,
         )
         self.log("umap_loss", encoder_loss, prog_bar=True)
 
