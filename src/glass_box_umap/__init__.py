b138928bb8a555d3157dd2e94f9d71a586cacafd
Evan Kiefl <kiefl.evan@gmail.com>
2026-04-13 13:42:46 -0700

    Keep our highest level API pure

--- src/glass_box_umap/__init__.py
+++ src/glass_box_umap/__init__.py
@@ -1,10 +1,7 @@
-from .attribution import compute_gene_contributions, verify_gene_reconstruction
 from .core import GlassBoxUMAP
 from .parametric_umap import ParametricUMAP
 
 __all__ = [
     "GlassBoxUMAP",
     "ParametricUMAP",
-    "compute_gene_contributions",
-    "verify_gene_reconstruction",
 ]
