8b7e5a9787a34f9b7525d3ea23d4b5082998e6f8
Evan Kiefl <kiefl.evan@gmail.com>
2026-05-22 16:16:06 -0700

    Add __version__ to glass_box_umap
    Closes #15

--- src/glass_box_umap/__init__.py
+++ src/glass_box_umap/__init__.py
@@ -1,8 +1,13 @@
+import importlib.metadata
+
 from .core import GlassBoxUMAP
 from .parametric_umap import ParametricUMAP
 from .parametric_umap import logging_config as logging_config
 
+__version__ = importlib.metadata.version("glass-box-umap")
+
 __all__ = [
     "GlassBoxUMAP",
     "ParametricUMAP",
+    "__version__",
 ]
