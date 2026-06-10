04f0c2e82d0cc84510ceb3f2fe86e48dda8d9a04
Evan Kiefl <ekiefl@users.noreply.github.com>
2026-06-10 13:09:13 -0700

    Add `__version__` attribute (#21)
    * Add __version__ to glass_box_umap
    
    Closes #15
    
    * Add lazy version

--- src/glass_box_umap/__init__.py
+++ src/glass_box_umap/__init__.py
@@ -1,6 +1,15 @@
+import importlib.metadata
+
 from .core import GlassBoxUMAP
 from .parametric_umap import ParametricUMAP
 from .parametric_umap import logging_config as logging_config
+
+
+def __getattr__(name: str) -> str:
+    if name == "__version__":
+        return importlib.metadata.version("glass-box-umap")
+    raise AttributeError(name)
+
 
 __all__ = [
     "GlassBoxUMAP",
